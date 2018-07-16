6405c5f2ee6f0cd20301cb390d585810f36a6b76
Harsh Murari <harsh@rhoynar.com>
2018-07-15 22:39:42 -0600

    Initial commit for BDD quickstart

---
+++ steps/calculator2.py
@@ -0,0 +1,14 @@
+# from behave import given, when, then
+# from calc import run
+
+# @given(u'Calculator app is run')
+# def step_impl(context):
+#     raise NotImplementedError(u'STEP: Given Calculator app is run')
+
+# @when(u'I input "{inp}" to calculator')
+# def step_impl(context, inp):
+#     raise NotImplementedError(u'STEP: When I input "2+3" to calculator')
+
+# @then(u'I get result "{out}"')
+# def step_impl(context, out):
+#     raise NotImplementedError(u'STEP: Then I get result "5"')
